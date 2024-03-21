add96925cc2827493eaaac67c9622c901d6c51a1
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-21 22:39:08 +0000

    feat: style button

--- src/LoginRegister/Button.tsx
+++ src/LoginRegister/Button.tsx
@@ -6,5 +6,12 @@ interface ButtonProps {
 }
 
 export const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
-  return <button onClick={onClick}>{text}</button>;
+  return (
+    <button
+      onClick={onClick}
+      className="bg-gradient-to-r from-bpink to-bpurple text-bwhite py-2 px-4 rounded-md font-montserrat text-lg font-semibold hover:bg-bpurple w-3/4"
+    >
+      {text}
+    </button>
+  );
 };
