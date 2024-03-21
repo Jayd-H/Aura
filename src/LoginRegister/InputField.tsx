9d11cf31731c8932f3359206644b3ccffc41591d
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-21 22:39:23 +0000

    feat: add placeholder text and icons

--- src/LoginRegister/InputField.tsx
+++ src/LoginRegister/InputField.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { EnvelopeIcon, LockClosedIcon } from "@heroicons/react/24/outline";
 
 interface InputFieldProps {
   label: string;
@@ -13,15 +14,29 @@ export const InputField: React.FC<InputFieldProps> = ({
   placeholder,
   required,
 }) => {
+  const icon =
+    type === "email" ? (
+      <EnvelopeIcon className="w-5 h-5 text-bblack" />
+    ) : (
+      <LockClosedIcon className="w-5 h-5 text-bblack " />
+    );
+
   return (
-    <div className="input-field">
-      <label htmlFor={label}>{label}</label>
-      <input
-        id={label}
-        type={type}
-        placeholder={placeholder}
-        required={required}
-      />
+    <div className="relative w-3/4 mb-4">
+      <div className="bg-gradient-to-r from-bpink to-bpurple rounded-md p-1">
+        <div className="flex items-center bg-bwhite rounded-md">
+          <input
+            type={type}
+            placeholder=" "
+            className="w-full px-4 py-2 rounded-md bg-bwhite font-montserrat text-base"
+            required={required}
+          />
+          <span className="mr-4">{icon}</span>
+        </div>
+      </div>
+      <label className="absolute top-0 -mt-2 left-4 bg-bwhite text-bblack text-sm font-montserrat font-semibold">
+        {label}
+      </label>
     </div>
   );
 };
