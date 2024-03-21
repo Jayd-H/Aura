2a74802e62c47776fcf87299a8c504d0c6f5b78e
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-21 21:23:57 +0000

    chore: add custom fonts, sizes, and background

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,7 +10,32 @@ export default {
 				'bpurple': '#CA6EEB',
 				'bpink': '#FF7BAC',
 			},
+			 backgroundImage: theme => ({
+        'aura-gradient': `radial-gradient(circle at top left, ${theme('colors.bpink')}, ${theme('colors.bpurple')})`,
+      }),
+	  fontFamily: {
+        'qahiri': ['Qahiri', 'sans-serif'],
+        'concert': ['Concert One', 'cursive'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+      },
+	  fontSize: {
+		'xs': ['0.625rem', { lineHeight: '1rem' }], // 10px
+        'sm': ['0.8125rem', { lineHeight: '1rem' }], // 13px
+        'base': ['1rem', { lineHeight: '1.5rem' }], // 16px
+        'lg': ['1.25rem', { lineHeight: '1.75rem' }], // 20px
+        'xl': ['1.5625rem', { lineHeight: '2rem' }], // 25px
+        '2xl': ['1.9375rem', { lineHeight: '2.25rem' }], // 31px
+        '3xl': ['2.4375rem', { lineHeight: '2.5rem' }], // 39px
+        '4xl': ['3.0625rem', { lineHeight: '1' }], // 49px
+        '5xl': ['3.8125rem', { lineHeight: '1' }], // 61px
+	  },
 		},
 	},
-	plugins: [],
+	plugins: [
+    function ({ addBase, theme }) {
+      addBase({
+        'body': { backgroundColor: theme('colors.bwhite'), color: theme('colors.bblack') },
+      });
+    },
+	  ],
 }
