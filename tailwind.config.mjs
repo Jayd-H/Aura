1e2c57046d354f7eb5a5c3e049a5c5546f3c2075
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-21 20:22:22 +0000

    chore: add custom colours

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'bwhite': '#ECEBF9',
+				'bblack': '#333040',
+				'bblue': '#28A70FF',
+				'bpurple': '#CA6EEB',
+				'bpink': '#FF7BAC',
+			},
+		},
 	},
 	plugins: [],
 }
