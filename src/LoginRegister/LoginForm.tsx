88d50a0fe8edc8824fa6bafca528d72aa7ffe01b
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-21 22:40:25 +0000

    feat: add majority of major styling
    still need to iron out some areas

--- src/LoginRegister/LoginForm.tsx
+++ src/LoginRegister/LoginForm.tsx
@@ -1,25 +1,43 @@
 import React from "react";
 import { InputField } from "./InputField";
 import { Button } from "./Button";
+import { Divider } from "./Divider";
 
 export const LoginForm: React.FC = () => {
   return (
-    <form className="login-form">
-      <h2>Welcome Back</h2>
-      <p>Please sign in with your Aura account</p>
-      <InputField label="Email" type="email" required />
-      <InputField label="Password" type="password" required />
-      <div className="form-footer">
-        <label>
-          <input type="checkbox" />
-          Remember Me
-        </label>
-        <a href="#">Forgot Password?</a>
+    <div className="flex flex-col items-center justify-center min-h-screen font-montserrat">
+      <div className="w-full max-w-md text-center">
+        <h2 className="text-bpurple text-3xl mb-2 font-semibold">
+          Welcome Back
+        </h2>
+        <Divider />
+        <p className="text-bblack text-lg font-semibold mb-16">
+          Please sign in with your Aura account
+        </p>
+        <div className="flex flex-col items-center w-full">
+          <InputField label="Email" type="email" required />
+          <InputField label="Password" type="password" required />
+          <div className="flex justify-between items-center mb-10 -mt-3 w-3/4">
+            <label className="flex items-center text-bblack text-sm">
+              <input type="checkbox" className="mr-1 form-checkbox bg-bwhite" />
+              Remember Me
+            </label>
+            <a href="#" className="text-bpurple text-sm hover:underline">
+              Forgot Password?
+            </a>
+          </div>
+          <p className="text-bblack text-xs mb-1">
+            By login, you agree to our Terms & Conditions
+          </p>
+          <Button text="Login" />
+        </div>
+        <p className="text-bblack text-sm mt-6 font-semibold">
+          Don't Have an account?{" "}
+          <a href="#" className="text-bpurple hover:underline">
+            Register
+          </a>
+        </p>
       </div>
-      <Button text="Login" />
-      <div className="register-link">
-        Don't Have an account? <a href="#">Register</a>
-      </div>
-    </form>
+    </div>
   );
 };
