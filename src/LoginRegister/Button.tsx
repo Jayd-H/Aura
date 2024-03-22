85da1ca5a134b6597de05fbc85655a3dd10485f3
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-22 00:51:43 +0000

    feat: new gradient on hover :)

--- src/LoginRegister/Button.tsx
+++ src/LoginRegister/Button.tsx
@@ -9,7 +9,7 @@ export const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="bg-tb-gradient text-bwhite py-2 px-4 rounded-md font-montserrat text-lg font-semibold hover:bg-lr-gradient w-3/4"
+      className="bg-bt-gradient text-bwhite py-2 px-4 rounded-lg font-montserrat text-lg font-semibold hover:bg-tb-gradient-blue w-3/4"
     >
       {text}
     </button>
