4b3d36abdf3d84fd6fa8e9b0082efb475f250cb5
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-22 02:11:00 +0000

    chore: add ability to pass disabled

--- src/LoginRegister/Button.tsx
+++ src/LoginRegister/Button.tsx
@@ -3,12 +3,14 @@ import React from "react";
 interface ButtonProps {
   text: string;
   onClick?: () => void;
+  disabled?: boolean;
 }
 
-export const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+export const Button: React.FC<ButtonProps> = ({ text, onClick, disabled }) => {
   return (
     <button
       onClick={onClick}
+      disabled={disabled}
       className="bg-bt-gradient text-bwhite py-2 px-4 rounded-lg font-montserrat text-lg font-semibold hover:bg-tb-gradient-blue w-3/4"
     >
       {text}
