04dd3ac503e220531c6da3cf74b0159157720c4c
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-22 22:47:19 +0000

    chore: create basic mini cards

---
+++ src/Components/Home/MiniCards/TimeMiniCard.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ElectricalServicesOutlined } from "@mui/icons-material";
+
+type MiniInfoCardProps = {
+  label: string;
+  value: string;
+  unit?: string;
+};
+
+const TimeMiniCard: React.FC<MiniInfoCardProps> = ({ label, value, unit }) => {
+  return (
+    <div className="bg-white rounded-lg p-4 shadow-md flex items-center">
+      <ElectricalServicesOutlined className="text-gray-500 text-xl" />
+      <div className="ml-2">
+        <p className="text-sm font-semibold">{label}</p>
+        <p className="text-lg">
+          {value}
+          {unit}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TimeMiniCard;
