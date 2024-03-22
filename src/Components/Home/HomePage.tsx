7ee4c010e055c261a9f4d2b328ab41489c4d6d38
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-22 21:53:08 +0000

    feat: add navbar, greetingcard, and aurapresets to homepage

--- src/Components/Home/HomePage.tsx
+++ src/Components/Home/HomePage.tsx
@@ -1,18 +1,56 @@
-import { AuraPreset } from "./Gen/AuraPreset";
+import AuraPreset from "./Gen/AuraPreset";
+import NavBar from "./Gen/Navbar";
+import GreetingCard from "./Gen/GreetingCard";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen flex">
-      <div className="m-auto">
-        <AuraPreset
-          auraName="Focus Time"
-          room="Office Space"
-          time="2"
-          hue="Cool"
-          power={false}
-          temperature="28"
-          backgroundImage="../../public/AuraImages/focustime.png"
+    <div className="flex flex-col">
+      <NavBar currentTime="09:41 AM" />
+      <div className="max-w-6xl mx-auto">
+        {/* Container for central content */}
+        <GreetingCard
+          greeting="Good Morning, Jayden"
+          subtext="How would you like to start your day?"
         />
+        <div className="flex overflow-x-auto py-2 space-x-4">
+          {/* Container for horizontal presets */}
+          <AuraPreset
+            auraName="Focus Time"
+            room="Office Space"
+            time="2"
+            hue="Cool"
+            power={false}
+            temperature="28"
+            backgroundImage="../../AuraImages/focustime.png"
+          />
+          <AuraPreset
+            auraName="Morning Wake-Up"
+            room="Bedroom"
+            time="1"
+            hue="Warm"
+            power={false}
+            temperature="30"
+            backgroundImage="../../AuraImages/morningwake.png"
+          />
+          <AuraPreset
+            auraName="Cozy Evening"
+            room="Living Room"
+            time="3"
+            hue="Warm"
+            power={false}
+            temperature="33"
+            backgroundImage="../../AuraImages/cozyevening.png"
+          />
+          <AuraPreset
+            auraName="Movie Night"
+            room="Living Room"
+            time="2"
+            hue="Warm"
+            power={false}
+            temperature="34"
+            backgroundImage="../../AuraImages/movienight.png"
+          />
+        </div>
       </div>
     </div>
   );
