51b5b05afe6b4df08fa3bca29c8e73a9c2f3c7d7
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-22 12:18:21 +0000

    feat: create simple password strength bar
    functionality still does not work

---
+++ src/LoginRegister/PasswordStrengthBar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const PasswordStrengthBar: React.FC<{
+  password: string;
+  strengthCalculator: (password: string) => number;
+}> = ({ password, strengthCalculator }) => {
+  const strength = strengthCalculator(password);
+
+  return (
+    <div className="relative w-2/3 mt-4">
+      <label className="text-bblack text-sm font-montserrat font-semibold">
+        Password Strength
+      </label>
+      <div className={`bg-tb-gradient rounded-md p-1/2`}>
+        <div className="flex items-center bg-bwhite rounded-md">
+          <div className="w-full pt-2 rounded-md font-montserrat text-base">
+            <div className="h-2 rounded-full">
+              <div
+                className="h-full bg-green-500 rounded-full"
+                style={{ width: `${strength}%` }}
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
