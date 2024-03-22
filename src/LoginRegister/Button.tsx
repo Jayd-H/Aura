e74e696bf19feacf3856e868a91a62a6fa7a5da6
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-22 00:15:20 +0000

    chore: use new gradient class

--- src/LoginRegister/Button.tsx
+++ src/LoginRegister/Button.tsx
@@ -9,7 +9,7 @@ export const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="bg-gradient-to-r from-bpink to-bpurple text-bwhite py-2 px-4 rounded-md font-montserrat text-lg font-semibold hover:bg-bpurple w-3/4"
+      className="bg-tb-gradient text-bwhite py-2 px-4 rounded-md font-montserrat text-lg font-semibold hover:bg-lr-gradient w-3/4"
     >
       {text}
     </button>
