3d6e6c4cc6649898e65c5d4b44efeb425227c000
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-22 21:52:17 +0000

    feat: create greetingcard

---
+++ src/Components/Home/Gen/GreetingCard.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { CalendarMonthOutlined } from "@mui/icons-material";
+
+type GreetingCardProps = {
+  greeting: string;
+  subtext: string;
+};
+
+export const GreetingCard: React.FC<GreetingCardProps> = ({
+  greeting,
+  subtext,
+}) => {
+  return (
+    <div className="p-6 font-montserrat">
+      <h1 className="text-3xl font-bold text-gradient pb-2 -ml-5 mb-2">
+        {greeting}
+      </h1>
+      <p className="text-lg -mb-6 -ml-5">{subtext}</p>
+    </div>
+  );
+};
+
+export default GreetingCard;
