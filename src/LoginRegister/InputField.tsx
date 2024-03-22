49ca57c4b48ae90354a07fc27b99e183cecc60c5
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-22 00:51:28 +0000

    fix: no bad black border on focus, new gradient on hover

--- src/LoginRegister/InputField.tsx
+++ src/LoginRegister/InputField.tsx
@@ -8,6 +8,12 @@ interface InputFieldProps {
   required?: boolean;
 }
 
+const focusStyle = `
+  focus:ring-0 
+  focus:border-0
+  focus:outline-none
+`;
+
 export const InputField: React.FC<InputFieldProps> = ({
   label,
   type,
@@ -23,12 +29,14 @@ export const InputField: React.FC<InputFieldProps> = ({
 
   return (
     <div className="relative w-3/4 mb-4 mt-6">
-      <div className="bg-lr-gradient rounded-md p-1/2">
+      <div
+        className={`bg-lr-gradient rounded-md p-1/2 hover:bg-lr-gradient-blue focus:bg-lr-gradient-blue ${focusStyle}`}
+      >
         <div className="flex items-center bg-bwhite rounded-md">
           <input
             type={type}
             placeholder=" "
-            className="w-full px-4 py-2 rounded-md bg-bwhite font-montserrat text-base"
+            className={`w-full px-4 py-2 rounded-md bg-bwhite font-montserrat text-base ${focusStyle}`}
             required={required}
           />
           <span className="mr-4">{icon}</span>
