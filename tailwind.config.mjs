244277389f3e61c2ccaf1b0683dd480c8cd201aa
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-24 19:41:55 +0000

    chore: remove comment

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -29,7 +29,7 @@ export default {
      boxShadow: {
         'inner-lg': 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06), inset 0 2px 6px 0 rgba(0, 0, 0, 0.1)', 
         'lg': '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)', 
-        'inner': 'inset 0 0 4px rgba(0, 0, 0, 0.25)', // Simulates inner shadow
+        'inner': 'inset 0 0 4px rgba(0, 0, 0, 0.25)',
         'even': '0 0 6px rgba(0, 0, 0, 0.25)',
      },
 			 backgroundImage: theme => ({
