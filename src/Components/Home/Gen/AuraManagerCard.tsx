46fbb3ce2dd405c0af2352b745ccf2bfbce4cc25
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-25 22:30:30 +0000

    feat: create aura manager card

---
+++ src/Components/Home/Gen/AuraManagerCard.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { AddBoxOutlined } from "@mui/icons-material";
+
+const AuraManagerButton: React.FC = () => {
+  const handleButtonClick = () => {
+    // Logic goes here
+  };
+
+  return (
+    <div>
+      <button
+        className="ml-16 bg-tb-gradient rounded-lg aura-manager-card font-montserrat font-bold text-lg text-bwhite"
+        onClick={handleButtonClick}
+      >
+        <p className="text-sm font-comfortaa mr-6 -mb-1">Add & Delete Auras</p>
+        <p className="pl-4">
+          Aura Manager <AddBoxOutlined className="w-10 h-10 -mt-4 " />
+        </p>
+      </button>
+    </div>
+  );
+};
+
+export default AuraManagerButton;
