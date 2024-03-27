7775793dfaa46bafd2c31a427df1292b52eef003
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-27 22:43:50 +0000

    feat: cool hover and click animation

--- src/Components/Home/LightMenu/Lights/LightButton.tsx
+++ src/Components/Home/LightMenu/Lights/LightButton.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 interface LightButtonProps {
   onClick: () => void;
@@ -7,12 +8,19 @@ interface LightButtonProps {
 
 const LightButton: React.FC<LightButtonProps> = ({ onClick, children }) => {
   return (
-    <button
-      className="mt-1 w-32 py-1 -mb-1 bg-bwhite text-bblack rounded-lg font-bold border-bblack hover:bg-bblack hover:text-bwhite"
+    <motion.button
       onClick={onClick}
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.95 }}
+      transition={{ duration: 0.2 }}
+      className="relative inline-block mt-1 w-32 py-1 -mb-1 rounded-lg font-bold group"
     >
-      {children}
-    </button>
+      <span className="relative z-10 block px-5 py-2 overflow-hidden font-medium leading-tight text-bblack transition-colors duration-300 ease-out border-bblack rounded-lg group-hover:text-bwhite">
+        <span className="absolute inset-0 w-full h-full px-5 py-2 rounded-lg bg-bwhite"></span>
+        <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-bblack group-hover:-rotate-180 ease"></span>
+        <span className="relative">{children}</span>
+      </span>
+    </motion.button>
   );
 };
 
