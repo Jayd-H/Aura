f1ad6d5868e17c84dc9db248a2ebbb1959d9c27a
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-27 02:31:54 +0000

    chore: move handlers outside homepage

---
+++ src/Components/Home/handlers.ts
@@ -0,0 +1,41 @@
+import type { Room, Light } from "./types";
+
+export const handleRoomTemperatureChange = (setRooms: React.Dispatch<React.SetStateAction<Room[]>>, roomName: string, newTemp: number): void => {
+  setRooms((prevRooms) =>
+    prevRooms.map((room) =>
+      room.name === roomName ? { ...room, temperature: newTemp } : room
+    )
+  );
+};
+
+export const handleRoomSelection = (setSelectedRoom: React.Dispatch<React.SetStateAction<Room | null>>, room: Room): void => {
+  console.log("handleRoomSelection called with room:", room); 
+  console.log("**** ROOM CHANGE ****"); // Added for debugging
+  setSelectedRoom(room);
+};
+
+
+export const handleLightSelection = (setSelectedLight: React.Dispatch<React.SetStateAction<Light | null>>, light: Light): void => {
+  setSelectedLight(light);
+};
+
+export const updateLightStatus = (
+    setRooms: React.Dispatch<React.SetStateAction<Room[]>>,
+    roomName: string,
+    lightName: string,
+    newStatus: boolean
+) => {
+    console.log("updateLightStatus called with:", roomName, lightName, newStatus);
+    setRooms((prevRooms) => 
+        prevRooms.map((room) =>
+            room.name === roomName
+            ? {
+                ...room,
+                lights: room.lights.map((light) =>
+                    light.name === lightName ? { ...light, isOn: newStatus } : light
+                ),
+            }
+            : room
+        )
+    );
+};
