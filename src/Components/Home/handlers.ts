7ea4e73040c050d39e4490f04b70e6051337fae0
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-27 15:22:24 +0000

    feat: create new functions for light toggle and colour

--- src/Components/Home/handlers.ts
+++ src/Components/Home/handlers.ts
@@ -15,60 +15,38 @@ export const handleRoomSelection = (setSelectedRoom: React.Dispatch<React.SetSta
 };
 
 
-export const handleLightSelection = (setSelectedLight: React.Dispatch<React.SetStateAction<Light | null>>, light: Light): void => {
-  setSelectedLight(light);
-};
-
 export const updateLightStatus = (
-    setRooms: React.Dispatch<React.SetStateAction<Room[]>>,
-    roomName: string,
-    lightName: string,
-    newStatus: boolean
-) => {
-    console.log("updateLightStatus called with:", roomName, lightName, newStatus);
-    setRooms((prevRooms) => 
-        prevRooms.map((room) =>
-            room.name === roomName
-            ? {
-                ...room,
-                lights: room.lights.map((light) =>
-                    light.name === lightName ? { ...light, isOn: newStatus } : light
-                ),
-            }
-            : room
-        )
-    );
+  rooms: Room[],
+  roomName: string,
+  lightName: string,
+  newStatus: boolean
+): Room[] => {
+  return rooms.map((room) =>
+    room.name === roomName
+      ? {
+          ...room,
+          lights: room.lights.map((light) =>
+            light.name === lightName ? { ...light, isOn: newStatus } : light
+          ),
+        }
+      : room
+  );
 };
 
-export const handleLightColourChange = (
-  setRooms: React.Dispatch<React.SetStateAction<Room[]>>,
-  setSelectedRoom: React.Dispatch<React.SetStateAction<Room | null>>,
-  selectedRoom: Room | null,
-  selectedLight: Light | null,
+export const updateLightColour = (
+  rooms: Room[],
+  roomName: string,
+  lightName: string,
   newColour: string
-): void => {
-  if (selectedRoom && selectedLight) {
-    setSelectedRoom((prevSelectedRoom) => {
-      if (prevSelectedRoom) {
-        const updatedLights = prevSelectedRoom.lights.map((light) =>
-          light.name === selectedLight.name ? { ...light, color: newColour } : light
-        );
-        return { ...prevSelectedRoom, lights: updatedLights };
-      }
-      return prevSelectedRoom;
-    });
-
-    setRooms((prevRooms) =>
-      prevRooms.map((room) =>
-        room.name === selectedRoom.name
-          ? {
-              ...room,
-              lights: room.lights.map((light) =>
-                light.name === selectedLight.name ? { ...light, color: newColour } : light
-              ),
-            }
-          : room
-      )
-    );
-  }
+): Room[] => {
+  return rooms.map((room) =>
+    room.name === roomName
+      ? {
+          ...room,
+          lights: room.lights.map((light) =>
+            light.name === lightName ? { ...light, color: newColour } : light
+          ),
+        }
+      : room
+  );
 };
