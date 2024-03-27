fbe90d9564bbee32db7947dbaea1c1cd25bf0da0
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-27 02:31:27 +0000

    chore: move data outside homepage

---
+++ src/Components/Home/data.ts
@@ -0,0 +1,68 @@
+export const roomsData = [
+    {
+        name: "Living Room",
+        temperature: 22,
+        lights: [
+            { id: "1", name: "Main Light", wattage: 60, isOn: true, color: "#ffffff" },
+            { id: "2", name: "Lamp", wattage: 40, isOn: true, color: "#ffff00" },
+            { id: "3", name: "Desk Light", wattage: 30, isOn: false, color: "#0000ff" },
+            { id: "4", name: "Main Light 2", wattage: 60, isOn: true, color: "#ffffff" },
+            { id: "5", name: "Lamp 2", wattage: 40, isOn: true, color: "#ffff00" },
+            { id: "6", name: "Desk Light 2", wattage: 30, isOn: false, color: "#0000ff" },
+            { id: "7", name: "Lamp 3", wattage: 40, isOn: true, color: "#ffff00" },
+            { id: "8", name: "Desk Light 3", wattage: 30, isOn: false, color: "#0000ff" },
+        ],
+    },
+    { 
+        name: "Dining Room", 
+        temperature: 21, 
+        lights: [
+            { id: "9", name: "Main Light", wattage: 60, isOn: true, color: "#ffffff" },
+        ] 
+    },
+    { 
+        name: "Master Bedroom", 
+        temperature: 20, 
+        lights: [
+            { id: "10", name: "Main Light", wattage: 60, isOn: true, color: "#ffffff" },
+            { id: "11", name: "Desk Light", wattage: 30, isOn: false, color: "#0000ff" },
+        ] 
+    },
+    { 
+        name: "Guest Bedroom", 
+        temperature: 21, 
+        lights: [
+            { id: "12", name: "Main Light", wattage: 60, isOn: true, color: "#ffffff" },
+        ] 
+    },
+    { 
+        name: "Kitchen", 
+        temperature: 23, 
+        lights: [
+            { id: "13", name: "Main Light", wattage: 60, isOn: true, color: "#ffffff" },
+            { id: "14", name: "Desk Light", wattage: 30, isOn: false, color: "#0000ff" },
+        ] 
+    },
+    { 
+        name: "Office Space", 
+        temperature: 24, 
+        lights: [
+            { id: "15", name: "Main Light", wattage: 60, isOn: true, color: "#ffffff" },
+            { id: "16", name: "Desk Light", wattage: 30, isOn: false, color: "#0000ff" },
+        ] 
+    },
+    { 
+        name: "Bathroom", 
+        temperature: 19, 
+        lights: [
+            { id: "17", name: "Main Light", wattage: 60, isOn: true, color: "#ffffff" },
+        ] 
+    },
+    { 
+        name: "Balcony", 
+        temperature: 18, 
+        lights: [
+            { id: "18", name: "Main Light", wattage: 60, isOn: true, color: "#ffffff" },
+        ] 
+    },
+];
