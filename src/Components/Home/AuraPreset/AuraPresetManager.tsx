b4542096926e7b5413740267dbfd5781a9328d5e
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-28 11:52:18 +0000

    feat: add styling for mobile

--- src/Components/Home/AuraPreset/AuraPresetManager.tsx
+++ src/Components/Home/AuraPreset/AuraPresetManager.tsx
@@ -3,34 +3,37 @@ import AuraPreset from "./AuraPreset";
 
 const AuraPresetManager: React.FC = () => {
   return (
-    <div className="flex overflow-x-auto py-6 space-x-4">
-      <AuraPreset
-        auraName="Focus Time"
-        room="Office Space"
-        time="2"
-        hue="Cool"
-        power={true}
-        temperature="28"
-        backgroundImage="../../AuraImages/focustime.png"
-      />
-      <AuraPreset
-        auraName="Morning Wake"
-        room="Bedroom"
-        time="1"
-        hue="Warm"
-        power={false}
-        temperature="30"
-        backgroundImage="../../AuraImages/morningwake.png"
-      />
-      <AuraPreset
-        auraName="Cozy Evening"
-        room="Living Room"
-        time="3"
-        hue="Warm"
-        power={false}
-        temperature="33"
-        backgroundImage="../../AuraImages/cozyevening.png"
-      />
+    <div className="container mx-auto">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 justify-items-center">
+        {/* Aura Preset components */}
+        <AuraPreset
+          auraName="Focus Time"
+          room="Office Space"
+          time="2"
+          hue="Cool"
+          power={true}
+          temperature="28"
+          backgroundImage="../../AuraImages/focustime.png"
+        />
+        <AuraPreset
+          auraName="Morning Wake"
+          room="Bedroom"
+          time="1"
+          hue="Warm"
+          power={false}
+          temperature="30"
+          backgroundImage="../../AuraImages/morningwake.png"
+        />
+        <AuraPreset
+          auraName="Cozy Evening"
+          room="Living Room"
+          time="3"
+          hue="Warm"
+          power={false}
+          temperature="33"
+          backgroundImage="../../AuraImages/cozyevening.png"
+        />
+      </div>
     </div>
   );
 };
