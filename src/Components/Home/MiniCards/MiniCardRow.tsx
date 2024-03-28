56e195ffc237981f81dbca88f3ac62f3a03726b3
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-28 11:53:12 +0000

    feat: import new mini card component + style for mobile view

--- src/Components/Home/MiniCards/MiniCardRow.tsx
+++ src/Components/Home/MiniCards/MiniCardRow.tsx
@@ -1,16 +1,36 @@
 import React from "react";
-import PowerMiniCard from "./PowerMiniCard";
-import TimeMiniCard from "./TimeMiniCard";
-import WeatherMiniCard from "./WeatherMiniCard";
-import DateMiniCard from "./DateMiniCard";
+import {
+  CalendarMonthOutlined,
+  ElectricalServicesOutlined,
+  AlarmOutlined,
+  CloudOutlined,
+} from "@mui/icons-material";
+import MiniCard from "./MiniCard";
 
 const MiniCardRow: React.FC = () => {
   return (
-    <div className="grid grid-cols-2 gap-4 max-w-none">
-      <DateMiniCard value="Friday" month="03" day="15" />
-      <TimeMiniCard value="9:41" unit="AM" />
-      <WeatherMiniCard value="31" unit="Raining" />
-      <PowerMiniCard value="360" unit="kW" />
+    <div className="grid md:grid-cols-4 grid-cols-2 gap-3 lg:grid-cols-2 md:max-w-none auto-rows-fr p-1">
+      <MiniCard
+        value="15"
+        subheading="Friday"
+        icon={<CalendarMonthOutlined className="text-md" />}
+        additionalText="03"
+      />
+      <MiniCard
+        value="9:41"
+        subheading="AM"
+        icon={<AlarmOutlined className="text-md" />}
+      />
+      <MiniCard
+        value="31°"
+        subheading="Raining"
+        icon={<CloudOutlined className="text-md" />}
+      />
+      <MiniCard
+        value="360"
+        subheading="kW"
+        icon={<ElectricalServicesOutlined className="text-md" />}
+      />
     </div>
   );
 };
