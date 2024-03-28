faa2e066497604825eec63925187f656edcb0ae2
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-28 11:55:31 +0000

    feat: style for mobile view

--- src/Components/Home/RoomSelectorRow/RoomSelector.tsx
+++ src/Components/Home/RoomSelectorRow/RoomSelector.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import RoomSelectButton from "./RoomSelectButton";
-import { AddOutlined } from "@mui/icons-material";
+import RoomSelectorPlus from "./RoomSelectorPlus";
 import type { Room } from "../types";
 
 interface RoomSelectorProps {
@@ -18,35 +18,16 @@ const RoomSelector: React.FC<RoomSelectorProps> = ({
     onRoomSelect(room);
   };
 
-  const handleButtonClick = () => {};
-
   return (
-    <div style={{ position: "relative" }}>
-      <div className="room-selector-container">
-        {rooms.map((room) => (
-          <RoomSelectButton
-            key={room.name}
-            room={room}
-            isSelected={selectedRoom?.name === room.name}
-            onClick={handleRoomSelection}
-          />
-        ))}
-      </div>
-      <button
-        data-tip="Add Room"
-        style={{
-          width: "25px",
-          height: "25px",
-          position: "absolute",
-          right: -45,
-          top: 25,
-        }}
-        className="border-full bg-lr-gradient items-center justify-center flex text-bwhite rounded-full hover:bg-lr-gradient-blue hover:shadow-2xl"
-        onClick={handleButtonClick}
-        title="Add Room"
-      >
-        <AddOutlined className="w-6 h-6" />
-      </button>
+    <div className="grid lg:grid-cols-4 grid-cols-2 gap-1">
+      {rooms.map((room) => (
+        <RoomSelectButton
+          key={room.name}
+          room={room}
+          isSelected={selectedRoom?.name === room.name}
+          onClick={handleRoomSelection}
+        />
+      ))}
     </div>
   );
 };
