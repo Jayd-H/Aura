14226e977d731b3bccbd54334466e4e7ef8e8515
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-03-28 19:20:34 +0000

    feat: mobile responsive!

--- src/Components/LoginRegister/LoginPage.tsx
+++ src/Components/LoginRegister/LoginPage.tsx
@@ -4,12 +4,16 @@ import { LoginForm } from "./LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex">
-      <div className="flex-1 bg-aura-gradient">
-        <InfoSection />
-      </div>
-      <div className="bg-bwhite pr-16 pl-16">
-        <LoginForm />
+    <div className="min-h-screen flex overflow-y-hidden">
+      <div className="w-full xl:grid xl:grid-cols-6">
+        <div className="hidden xl:block xl:col-span-4 bg-aura-gradient">
+          <InfoSection />
+        </div>
+        <div className="flex justify-center items-center xl:col-span-2">
+          <div className="w-full max-w-md p-8 bg-bwhite">
+            <LoginForm />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Components/LoginRegister/RegisterPage.tsx
+++ src/Components/LoginRegister/RegisterPage.tsx
@@ -4,12 +4,16 @@ import { RegisterForm } from "./RegisterForm";
 
 export default function RegisterPage() {
   return (
-    <div className="min-h-screen flex">
-      <div className="flex-1 bg-aura-gradient">
-        <InfoSection />
-      </div>
-      <div className="bg-bwhite pr-16 pl-16">
-        <RegisterForm />
+    <div className="min-h-screen flex overflow-y-hidden">
+      <div className="w-full xl:grid xl:grid-cols-6">
+        <div className="hidden xl:block xl:col-span-4 bg-aura-gradient">
+          <InfoSection />
+        </div>
+        <div className="flex justify-center items-center xl:col-span-2">
+          <div className="w-full max-w-md p-8 bg-bwhite">
+            <RegisterForm />
+          </div>
+        </div>
       </div>
     </div>
   );
