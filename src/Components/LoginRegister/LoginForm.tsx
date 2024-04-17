1c84c9231dc40b6466ff534495c1d57fcbaa1969
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-04-17 12:31:04 +0100

    feat: first input field focused on load

--- src/Components/LoginRegister/LoginForm.tsx
+++ src/Components/LoginRegister/LoginForm.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { InputField } from "./InputField";
 import { Button } from "../Button";
 import { Divider } from "./Divider";
 
 export const LoginForm: React.FC = () => {
+  const emailInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    emailInputRef.current?.focus();
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen font-montserrat">
       <div className="w-full max-w-md text-center">
@@ -15,8 +21,19 @@ export const LoginForm: React.FC = () => {
           <i>Please sign in with your Aura account</i>
         </p>
         <div className="flex flex-col items-center w-full">
-          <InputField label="Email" type="email" required />
-          <InputField label="Password" type="password" required />
+          <InputField
+            label="Email"
+            type="email"
+            placeholder="Email"
+            required
+            ref={emailInputRef}
+          />
+          <InputField
+            label="Password"
+            type="password"
+            placeholder="Password"
+            required
+          />
           <div className="flex justify-between items-center mb-14 -mt-3 w-3/4">
             <label className="flex items-center text-bblack text-sm">
               <input type="checkbox" className="mx-1 form-checkbox bg-bwhite" />
@@ -32,7 +49,6 @@ export const LoginForm: React.FC = () => {
           <p className="text-bblack text-xs mb-1">
             By login, you agree to our Terms & Conditions
           </p>
-
           <Button text="Login" navigateTo="/home" />
         </div>
         <p className="text-bblack text-sm mt-6 font-semibold">
