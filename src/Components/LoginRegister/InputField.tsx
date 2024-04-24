ad5277491a816d3489063d5bacb9b5961121a586
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-04-24 16:53:56 +0100

    feat: add password toggle button for password inputs

--- src/Components/LoginRegister/InputField.tsx
+++ src/Components/LoginRegister/InputField.tsx
@@ -1,5 +1,10 @@
-import React from "react";
-import { EnvelopeIcon, LockClosedIcon } from "@heroicons/react/24/outline";
+import React, { useState } from "react";
+import {
+  EnvelopeIcon,
+  LockClosedIcon,
+  EyeIcon,
+  EyeSlashIcon,
+} from "@heroicons/react/24/outline";
 
 interface InputFieldProps {
   label: string;
@@ -18,12 +23,30 @@ const focusStyle = `
 
 export const InputField = React.forwardRef<HTMLInputElement, InputFieldProps>(
   ({ label, type, placeholder, required, value, onChange }, ref) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+      setShowPassword((prevState) => !prevState);
+    };
+
     const icon =
       type === "email" ? (
         <EnvelopeIcon className="w-5 h-5 text-bblack" />
       ) : (
         <LockClosedIcon className="w-5 h-5 text-bblack" />
       );
+
+    const passwordIcon = showPassword ? (
+      <EyeSlashIcon
+        className="w-5 h-5 text-bblack cursor-pointer"
+        onClick={togglePasswordVisibility}
+      />
+    ) : (
+      <EyeIcon
+        className="w-5 h-5 text-bblack cursor-pointer"
+        onClick={togglePasswordVisibility}
+      />
+    );
 
     return (
       <div className="relative w-3/4 mb-4 mt-6">
@@ -32,7 +55,7 @@ export const InputField = React.forwardRef<HTMLInputElement, InputFieldProps>(
         >
           <div className="flex items-center bg-white rounded-md">
             <input
-              type={type}
+              type={showPassword ? "text" : type}
               placeholder={placeholder}
               className={`w-full px-4 py-2 rounded-md bg-white font-montserrat text-base ${focusStyle}`}
               required={required}
@@ -40,7 +63,9 @@ export const InputField = React.forwardRef<HTMLInputElement, InputFieldProps>(
               onChange={onChange}
               ref={ref}
             />
-            <span className="mr-4">{icon}</span>
+            <span className="mr-4">
+              {type === "password" ? passwordIcon : icon}
+            </span>
           </div>
         </div>
         <label className="absolute top-0 -mt-4 left-2 bg-bwhite border-b-2 border-bblack px-1 border-dashed text-bblack text-sm font-montserrat font-semibold">
